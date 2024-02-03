513f8a5eeff3bbed2f1f1da7fb073c1098066993
echelon rogue1ai <echelon@rogue1.ai>
2024-02-02 19:22:23 -0500

    run_test.py

---
+++ run_test.py
@@ -0,0 +1,23 @@
+
+# this file should run a test script to exercise all the functions in 
+# jirasimplelib
+#
+# create a project
+# create epics: ep-1, ep-2, ep-3
+# create stories s-1 to s-30
+# move stories s-1 to s-15 to ep-1, s-16 to s-20 to ep-2 an s-21 to s-30 to ep-3
+# create sprints sp-1 tp sp-3
+# add s-1 to s10 to sp-1
+# add s-11 to s20 to sp-2
+# add s-21 to s30 to sp-3
+# start sp-1
+# add comments to s-1 to s-15
+# get list of stories in sp-1
+# move s-10 to s-15 to s-2
+# complete all stories in sp-1
+# complete sp-1
+# start sp-2
+# add comment to all stories in sp-2
+# delete all stories
+# delete all sptrints
+# delete all projects
