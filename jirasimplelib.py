bfdc77d1f4561416ebe3f9e7763be004d7573e68
rimshaveritus <rimsha.ashfaq@verituslabs.com>
2024-05-13 12:03:15 +0500

    13-05-2024

--- jirasimplelib.py
+++ jirasimplelib.py
@@ -249,42 +249,19 @@ def update_story_description(jira, story_key, new_description):
 # Function to update a story's assignee
 def update_story_assignee(jira, story_key, new_assignee):
     try:
-        # Construct the API endpoint
-        endpoint = f"{jira._options['server']}/rest/api/3/issue/{story_key}"
-
-        # Prepare the request headers
-        headers = {"Accept": "application/json", "Content-Type": "application/json"}
-
-        # Construct the request body with the new assignee or None
-        payload = {
-            "fields": {"assignee": {"name": new_assignee} if new_assignee else None}
-        }
-
-        # Send the PUT request to update the assignee
-        response = requests.put(
-            endpoint,
-            headers=headers,
-            json=payload,
-            auth=(
-                jira._session.auth[0],
-                jira._session.auth[1],
-            ),  # HTTP Basic Authentication
-        )
-
-        # Check if the request was successful
-        if response.status_code == 204:
-            print(f"Story assignee updated successfully. Key: {story_key}")
-            return story_key
-        else:
-            print(
-                f"Failed to update story assignee. Key: {story_key}. Status code: {response.status_code}"
-            )
+        story = jira.issue(story_key)
+        if story is None:
+            logging.error(f"Error: Issue with key {story_key} not found.")
             return None
-
+        
+        story.update(assignee=new_assignee)
+        assignee_name = story.fields.assignee.name
+        logging.info(f"Story assignee updated successfully. Key: {story_key}")
+        return assignee_name
     except JIRAError as e:
-        # Print error message if an exception occurs
-        print(f"Error updating story assignee: {e}")
+        logging.error(f"Error updating story assignee: {e}")
         return None
+
 
 
 def print_issue_assignee(jira, issue_key):
